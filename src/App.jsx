db89c60cb15a7c802a8d1a537297dfd0ec679b3e
Tyler Smith <tylersmith4596@gmail.com>
2024-09-15 22:45:01 -0700

    Refactor App component to add ShoppingCart route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./pages/LandingPage/LandingPage";
 import ProductListing from "./pages/ProductListing/ProductListing";
 import ProductDetails from "./pages/ProductDetails/ProductDetails";
 import NavBar from "./components/NavBar/NavBar";
+import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
 // import Footer from "./components/Footer";
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/product-listing" element={<ProductListing />} />
         <Route path="/product/:productId" element={<ProductDetails />} />
-        <Route path="/shopping-cart" element={<h1>Shopping Cart</h1>} />
+        <Route path="/shopping-cart" element={<ShoppingCart />} />
       </Routes>
     </div>
   );
